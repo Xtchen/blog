b25386b4973eef48f76b17e0714528fac966a312
Hang Li <hangli@Hangs-MacBook-Air.local>
2013-06-20 22:17:13 -0700

    modified the css style ans some config staff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,6 @@ Blog::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
+  
 end
