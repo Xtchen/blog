1a36700df62043a2bf639fdbf3d0d256286183f4
xtchen <cxtaa@foxmail.com>
2013-07-08 18:16:49 -0700

    fix the time zone problem and some js stuff

--- app/assets/ckeditor/config.js
+++ app/assets/ckeditor/config.js
@@ -6,10 +6,33 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	config.width = 250;
-	config.uiColor = '#FFF';
-	config.toolbarGroups = [
-        { name: 'pbckcode' } ,                   // shows the pbckcode button
-    ];
-    config.extraPlugins = 'pbckcode';
+	
 };
+CKEDITOR.config.toolbarLocation = 'top';
+CKEDITOR.config.toolbarGroups = [
+ 			{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+ 			{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+ 			{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+ 			{ name: 'forms' },
+ 			'/',
+ 			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+ 			{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+ 			{ name: 'links' },
+ 			{ name: 'insert' },
+ 			'/',
+ 			{ name: 'styles' },
+ 			{ name: 'colors' },
+ 			{ name: 'tools' },
+ 			{ name: 'others' },
+ 			{ name: 'pbckcode' }
+ 		];
+ CKEDITOR.config.toolbar_Basic = [
+ 			['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink', '-', 'pbckcode']
+ 		];
+CKEDITOR.config.extraPlugins = 'pbckcode';
+CKEDITOR.config.pbckcode = {
+        modes :  [ ["C/C++", "c_pp"],['Java', 'java'] ],
+        theme : 'clouds_midnight',
+        highlighter : "PRETTIFY"
+    };
+//CKEDITOR.config.toolbar = 'Basic';
