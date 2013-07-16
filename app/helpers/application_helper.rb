adf2a212daf4407c6b77f2344e8132f6d4000890
xtchen <cxtaa@foxmail.com>
2013-07-16 16:48:18 -0700

    modified the logging system

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
 	def client_ip
   		request.remote_ip
   	end
+
+  	def record_ip(where)
+  		if !signed_in?
+  			Rails.logger.info "<record> #{client_ip} @#{Time.now} visited #{where} </record>"
+  		end
+  	end
 end
