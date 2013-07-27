e01732bff9e01eecfe6b1f859d14184d7de69cef
xtchen <cxtaa@foxmail.com>
2013-07-26 22:22:15 -0700

    modifing css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,74 +55,7 @@ $(document).ready(function(){
 		});
  		return false;
 	 });
- 	//  $('#new_comment').submit(function() {
- 	// 	var com = {
- 	// 	 author:$('#comment_author').val(),
- 	// 	 post_id:$('#comment_post_id').val(), 
- 	// 	 content:$('#comment_content').val()
- 	// 	};
-
-	 // 	var c={
-	 // 		comment:com
-	 // 	};
-
- 	// 	//alert("before");
- 	// 	$.post("/comments", c, 
- 	// 		function(data){
- 	// 			if(data.saved == true){
-	 // 				clean_content($('#comment_author'));
-		//  			clean_content($('#comment_content'));
-		//  			$('#all-comments').html("");//clean the original content
-		//  			// var jsonobj = eval('(' + data + ')');
-		//  			// $('#all-comments').append(jsonobj.count.toString());
-		//  			// //add the header
-		//  			var htmlstr="";
-		//  			if(data.count == 0){
-		//  				htmlstr += "<legend class=\"muted\">Still no comment.</legend>";
-		//  			}
-		//  			else{
-		//  				htmlstr += "<legend class=\"muted\">"+data.count.toString()+" Comments:</legend>";
-		//  			}
-
-	 // 				$.each(data.comments, function(i, item) {
-	 // 					htmlstr += "<div id=\"comment\">";
-	 // 					htmlstr += "<div id=\"comment-hidden-"+item.id.toString()+"\"></div>";
-	 // 					htmlstr += "<p class=\"muted normal-size\">";
-	 // 					if(item.mine == true){
-	 // 						htmlstr += "<span class=\"label label-important\">Author</span>";
-	 // 					}
-	 // 					htmlstr += "<strong> "+item.author+"</strong> @ "+item.created_at+"</p>";
-	 // 					htmlstr += "<p>"+item.content+"</p>";
-	 // 					if(data.valid == true){
-	 // 						htmlstr += "<div><a id=\"delete-btn-"+item.id.toString()+"\" href=\"/comments/"+item.id.toString()+"\" class=\"btn btn-primary\" data-confirm=\"Delete this comment?\" data-method=\"delete\" rel=\"nofollow\">Delete</a></div>";
-	 // 					}
-	 // 					htmlstr += "<hr>";
-	 // 					htmlstr += "</div>";
-	 // 				});
-	 // 				$('#all-comments').append(htmlstr);
-
-	 // 				var current_comment = "#comment-hidden-"+data.current.toString();
-	 // 				$("html,body").animate({scrollTop:$(current_comment).offset().top}, 1000);
-	 // 				$(current_comment).addClass("alert alert-success").html("<p>Comment posted successfully.</p>").show();
-	 // 				$(current_comment).parent().fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500, function(){
-	 // 					$(current_comment).fadeOut(5000);	
-	 // 				});
-	 // 			}
-	 // 			else{
-	 // 				$('#comment-error-message')
-	 // 				.addClass("alert alert-error")
-	 // 				.html("<p>Comment invalid. The comment should contain at least 6 characters. Thx!</p>")
-	 // 				.show()
-	 // 				.fadeOut(5000, function(){
-	 // 				$('#comment-error-message').removeClass("alert alert-error")
-	 // 				.html("")
-	 // 				});
-	 // 			}
- 	// 		} );
- 	// 	//alert("after");
- 	// 	return false;
- 	// });
-
+ 	
  	function clean_content(elem){
  		elem.val("")
  	}
@@ -154,21 +87,6 @@ $(document).ready(function(){
  		}
  		else{
  			$('#go-top').hide();
- 		}
- 		if (distance < topOf2ndH1){
- 			showImg(goNext);
- 			goTop.hide();
- 			goPrev.hide();
- 		}
- 		else if(distance < topOfLastH1){
- 			showImg(goTop);
- 			showImg(goPrev);
- 			showImg(goNext);
- 		}
- 		else{
- 			goNext.hide();
-			showImg(goPrev);
- 			showImg(goTop);
  		}
  	});
 
